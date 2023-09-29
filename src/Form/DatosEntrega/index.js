96cfd241d0c514248d7f7e7807f99278ff199ba6
Kenia Montalvo <kenlimonra@gmail.com>
2023-09-29 02:02:21 -0600

    se terminó por validar los datos de entrega

--- src/Form/DatosEntrega/index.js
+++ src/Form/DatosEntrega/index.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarInput } from "./validaciones";
 
 const DatosEntrega = ({updateStep}) => {
+
+  const [address, setAddress] = useState({value: '', valid: null})
+  const [city, setCity] = useState({value: '', valid: null})
+  const [province, setProvince] = useState({value: '', valid: null})
+
   return (
     <Box
       component="form"
@@ -23,6 +29,14 @@ const DatosEntrega = ({updateStep}) => {
         fullWidth
         margin="dense"
         type="text"
+        value={address.value}
+        onChange={ (input) => {
+          const value = input.target.value;
+          const valid = validarInput(value)
+          setAddress({value, valid})
+        }}
+        error={address.valid == false}
+        helperText={address.valid == false && "Ingresa al menos 4 caracteres"}
       />
       <TextField
         label="Ciudad"
@@ -30,6 +44,14 @@ const DatosEntrega = ({updateStep}) => {
         fullWidth
         margin="dense"
         type="text"
+        value={city.value}
+        onChange={ (input) => {
+          const value = input.target.value;
+          const valid = validarInput(value)
+          setCity({value, valid})
+        }}
+        error={city.valid == false}
+        helperText={city.valid == false && "Ingresa al menos 4 caracteres"}
       />
       <TextField
         label="Estado/Provincia"
@@ -37,6 +59,14 @@ const DatosEntrega = ({updateStep}) => {
         fullWidth
         margin="dense"
         type="text"
+        value={province.value}
+        onChange={ (input) => {
+          const value = input.target.value;
+          const valid = validarInput(value)
+          setProvince({value, valid})
+        }}
+        error={province.valid == false}
+        helperText={province.valid == false && "Ingresa al menos 4 caracteres"}
       />
       <Button variant="contained" type="submit">
         Crear cuenta
