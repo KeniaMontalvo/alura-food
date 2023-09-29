f44931ca86150da3a3ab8798f7cec4973981098c
Kenia Montalvo <kenlimonra@gmail.com>
2023-09-29 01:06:07 -0600

    Se selecciona el formulario

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
-// import DatosPersonales from "./DatosPersonales";
-// import DatosEntrega from "./DatosEntrega";
-// import Complete from "./Complete";
-// import Stepper from "../Stepper";
+import DatosPersonales from "./DatosPersonales";
+import DatosEntrega from "./DatosEntrega";
+import Complete from "./Complete";
+import Stepper from "../Stepper";
 
 const Form = () => {
+  const [step, setStep] = useState(0);
+
+  //step = 0 => <DatosUsuario />
+  //step = 1 => <DatosPersonales />
+  //step = 2 => <DatosEntrega />
+  //step = 3 => <Complete />
+
+  const steps = {
+    0: <DatosUsuario />,
+    1: <DatosPersonales />,
+    2: <DatosEntrega />,
+    3: <Complete />
+  }
+
   return (
     <Box
       sx={{
@@ -21,9 +35,11 @@ const Form = () => {
         <Typography variant="h3">AluraFood</Typography>
       </LogoSpace>
       <FormSpace>
-        <DatosUsuario />
+        { (step < 3) && <Stepper step={step} />}
+        {/* <DatosUsuario /> */}
         {/* <DatosPersonales />
         <DatosEntrega /> */}
+        {steps[step]}
       </FormSpace>
     </Box>
   );
