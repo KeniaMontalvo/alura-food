3bd152411f8fbdda9509171e2a8ed29de858c221
Kenia Montalvo <kenlimonra@gmail.com>
2023-09-29 19:54:19 -0600

    se usó un hook nuevo llamado useEffect

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
@@ -6,9 +6,34 @@ import DatosPersonales from "./DatosPersonales";
 import DatosEntrega from "./DatosEntrega";
 import Complete from "./Complete";
 import Stepper from "../Stepper";
+import Step from "./Step";
+
+//validaciones
+import { validarEmail, validarPassword } from "./DatosUsuario/validaciones";
 
 const Form = () => {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);
+  const [pasos, setPasos] = useState({});
+
+  useEffect(() => {
+    console.log("UseEffect")
+  });
+
+  useEffect(() => {
+    console.log("Se ha actualizado el step: ", step);
+  }, [step])
+
+  /*
+  useEffect(async () => {
+      try{
+        const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const posts = await data.json()
+        console.log(posts);
+      } catch(e){
+        console.log(e)
+      }
+  })
+  */
 
   //step = 0 => <DatosUsuario />
   //step = 1 => <DatosPersonales />
@@ -24,7 +49,78 @@ const Form = () => {
     0: <DatosUsuario updateStep={updateStep} />,
     1: <DatosPersonales updateStep={updateStep} />,
     2: <DatosEntrega updateStep={updateStep} />,
-    3: <Complete />
+    3: <Complete />,
+  };
+
+  console.log("FORM COMPONENT")
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let newStep = step + 1;
+    setStep(newStep)
+    console.log("NewStep", newStep)
+    console.log(step)
+  };
+
+  const handleChange = (element, position, currentStep, validator) => {
+    const value = element.target.value;
+    const valid = validator(value);
+    console.log(value);
+    console.log(valid);
+    console.log("position", position)
+    console.log("currentStep", currentStep)
+    console.log("validator", validator)
+  }
+
+  const stepsFlow = {
+    0: {
+      inputs: [
+        {
+          label: "Correo electrónico",
+          type: "email",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa un correo electrónico válido",
+          validator: validarEmail,
+        },
+        {
+          label: "Contraseña",
+          type: "password",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa una contraseña válida, al menos 8 caracteres y máximo 20.",
+          validator: validarPassword,
+        }
+      ],
+      buttonText: "Siguiente",
+      onSubmit,
+    },
+    1: {
+      inputs: [
+        {
+          label: "Correo electrónico",
+          type: "email",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa un correo electrónico válido",
+          validator: validarEmail,
+        },
+        {
+          label: "Contraseña",
+          type: "password",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperText: "Ingresa una contraseña válida, al menos 8 caracteres y máximo 20.",
+          validator: validarPassword,
+        }
+      ],
+      buttonText: "Siguiente",
+      onSubmit,
+    }
   }
 
   return (
@@ -40,11 +136,9 @@ const Form = () => {
         <Typography variant="h3">AluraFood</Typography>
       </LogoSpace>
       <FormSpace>
-        { (step < 3) && <Stepper step={step} />}
-        {/* <DatosUsuario /> */}
-        {/* <DatosPersonales />
-        <DatosEntrega /> */}
-        {steps[step]}
+        { step < 3 && <Stepper step={step} />}
+        {/* {steps[step]} */}
+        <Step data={stepsFlow[step]} step={step} />
       </FormSpace>
     </Box>
   );
